a62a0b21fe02436aed1d041a178f34d719c9e4df
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-15 07:47:14 -0500

    Ajout superclasse GestionnaireDomaine.

---
+++ mgdomaines/appareils/SenseurPassifs.py
@@ -0,0 +1,8 @@
+# Module avec les classes de donnees, processus et gestionnaire de sous domaine mgdomaines.appareils.SenseursPassifs
+from millegrilles.Domaines import GestionnaireDomaine
+
+
+class GestionnaireSenseursPassifs(GestionnaireDomaine):
+
+    def __init__(self):
+        super().__init__()
