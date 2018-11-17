f6abf1fc5aa2e07537723d7542f48d68390b4bb1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-16 19:02:01 -0500

    Ajout declencheur quotidien.

--- mgdomaines/appareils/DeclencheursAppareils.py
+++ mgdomaines/appareils/DeclencheursAppareils.py
@@ -1,18 +1,32 @@
 from millegrilles.Declencheur import Declencheur
-import sys, datetime
+import sys
+import datetime
+
 
 class DeclencheurSenseursPassif(Declencheur):
+
+    NOM_DOMAINE = 'mgdomaines.appareils.SenseursPassifs'
 
     def __init__(self):
         super().__init__()
 
     def maj_horaire(self):
-        domaine = 'mgdomaines.appareils.SenseursPassifs.MAJHoraire'
+        domaine = '%s.%s' % (self.NOM_DOMAINE, 'MAJHoraire')
         dict_message = {
             'evenements': 'miseajour.horaire',
             'timestamp': datetime.datetime.utcnow().isoformat()
         }
         self.transmettre_declencheur_domaine(domaine, dict_message)
+
+    def maj_quotidienne(self):
+        domaine = '%s.%s' % (self.NOM_DOMAINE, 'MAJQuotidienne')
+        dict_message = {
+            'evenements': 'miseajour.quotidienne',
+            'timestamp': datetime.datetime.utcnow().isoformat()
+        }
+        self.transmettre_declencheur_domaine(domaine, dict_message)
+
+# **** MAIN ****
 
 if len(sys.argv) == 2:
 
@@ -23,4 +37,4 @@ if len(sys.argv) == 2:
     methode_declencheur()
 
 else:
-    print("Il faut fournir le nom de la methode a executer")
+    print("Il faut fournir le nom de la methode a executer. Arguments fournis: %s" % sys.argv)
