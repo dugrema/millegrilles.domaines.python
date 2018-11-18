d592a042d3414d533a8399647303bec5a79cdb9e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-18 13:52:13 -0500

    Deplacement script gestionnaire vers millegrilles.
    Modification du script pour supporter arguments (script generique).

--- mgdomaines/appareils/script_demarrergestionnaire.py
+++
@@ -1,5 +0,0 @@
-from mgdomaines.appareils.SenseursPassifs import GestionnaireSenseursPassifs
-
-print("Demarrage du gestionnaire de Senseurs Passifs")
-gestionnaire = GestionnaireSenseursPassifs()
-gestionnaire.executer_gestionnaire()
