32a3c4fe72ddcbd3c792918f0b952caaa1e4bf5f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-18 13:39:08 -0500

    Creation script demarrage gestionnaire senseurs passifs.

---
+++ mgdomaines/appareils/script_demarrergestionnaire.py
@@ -0,0 +1,5 @@
+from mgdomaines.appareils.SenseursPassifs import GestionnaireSenseursPassifs
+
+print("Demarrage du gestionnaire de Senseurs Passifs")
+gestionnaire = GestionnaireSenseursPassifs()
+gestionnaire.executer_gestionnaire()
