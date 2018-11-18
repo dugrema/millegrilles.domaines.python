225323763f7df6b56a7b2ec8bce906006b45b5ed
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-18 11:24:18 -0500

    Ajout classe pour traiter backlog senseur passifs lectures.

--- mgdomaines/appareils/SenseursPassifs.py
+++ mgdomaines/appareils/SenseursPassifs.py
@@ -54,6 +54,9 @@ class GestionnaireSenseursPassifs(GestionnaireDomaine):
         )
 
     def traiter_backlog(self):
+        # Il faut trouver la transaction la plus recente pour chaque noeud/senseur et relancer une transaction
+        # de persistance.
+        # Toutes les autres transactions non-traitees de SenseursPassifs.Lecture peuvent etre marquees comme traitees.
         pass
 
     def get_nom_queue(self):
@@ -513,6 +516,78 @@ class ProcessusTransactionSenseursPassifsMAJQuotidienne(MGProcessus):
             producteur.calculer_aggregation_mois(doc_senseur)
 
         self.set_etape_suivante()  # Rien a faire, etape finale
+
+
+class TraitementBacklogLecturesSenseursPassifs():
+
+    def __init__(self, message_dao, document_dao):
+        self._message_dao = message_dao
+        self._document_dao = document_date
+
+    def run_requete_plusrecentetransactionlecture_parsenseur(self):
+        filtre = {
+            'info-transaction.domaine': SenseursPassifsConstantes.TRANSACTION_VALEUR_DOMAINE,
+            'evenements.transaction_traitee': {'$exists': False}
+        }
+
+        regroupement = {
+            '_id': {
+                'noeud': '$charge-utile.noeud',
+                'senseur': '$charge-utile.senseur'
+            },
+            'temps_lecture': {'$max': '$charge-utile.temps_lecture'}
+        }
+
+        operation = [
+            {'$match': filtre},
+            {'$group': regroupement}
+        ]
+
+        print("Operation aggregation: %s" % str(operation))
+
+        collection_transactions = self.document_dao.get_collection(Constantes.DOCUMENT_COLLECTION_TRANSACTIONS)
+        resultat_curseur = collection_transactions.aggregate(operation)
+
+        liste_transaction_senseurs = []
+        for res in resultat_curseur:
+            transaction = {
+                'noeud': res['_id']['noeud'],
+                'senseur': res['_id']['senseur'],
+                'temps_lecture': res['temps_lecture']
+            }
+            liste_transaction_senseurs.append(transaction)
+            print("Resultat: %s" % str(transaction))
+
+        return liste_transaction_senseurs
+
+    def run_requete_genererdeclencheur_parsenseur(self, liste_senseurs):
+
+        collection_transactions = self.document_dao.get_collection(Constantes.DOCUMENT_COLLECTION_TRANSACTIONS)
+
+        for transaction_senseur in liste_senseurs:
+            filtre = {
+                'charge-utile.noeud': transaction_senseur['noeud'],
+                'charge-utile.senseur': transaction_senseur['senseur'],
+                'charge-utile.temps_lecture': transaction_senseur['temps_lecture']
+            }
+            projection = {
+                '_id': 1
+            }
+            resultat_curseur = collection_transactions.find(filter=filtre, projection=projection)
+
+            for res in resultat_curseur:
+                # Preparer un message pour declencher la transaction
+                print("Transaction a declencher: _id = %s" % str(res['_id']))
+
+                # Marquer toutes les transaction anterieures comme traitees
+                filtre['evenements.transaction_traitee'] = {'$exists': False}
+                filtre['charge-utile.temps_lecture'] = {'$lt': transaction_senseur['temps_lecture']}
+
+                operation = {
+                    '$push': {'evenements.transaction_traitee': datetime.datetime.utcnow()}
+                }
+
+                collection_transactions.update_many(filter=filtre, update=operation)
 
 # Processus pour mettre a jour un document de noeud suite a une transaction de senseur passif
 class ProcessusMAJNoeudsSenseurPassif(MGProcessus):
