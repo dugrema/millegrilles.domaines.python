4d17fc9ae7efe332aa6b08a3971f915140f3ea94
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-22 14:24:14 -0500

    Modification setup.py domaines.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='MilleGrilles.domaines.python',
+    version='0.3',
+    packages=['mgdomaines',
+              'mgdomaines.appareils'],
+    url='',
+    license='',
+    author='Mathieu Dugre',
+    author_email='mail@mdugre.info',
+    description=''
+)
