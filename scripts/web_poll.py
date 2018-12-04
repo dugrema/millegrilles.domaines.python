641bb738265423cd3c3006f59de0c0dd784a669d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-04 12:34:06 -0500

    Separation code pour mettre methode main() dans la superclasse.
    Ajout parametre --debug pour logging.

--- scripts/web_poll.py
+++ scripts/web_poll.py
@@ -1,6 +1,5 @@
 # Script pour demarrer un web poll
 
-import traceback
 import logging
 
 from millegrilles.util.UtilScriptLigneCommande import ModeleAvecMessageDAO
@@ -9,7 +8,8 @@ from mgdomaines.web.WebPoll import WebPageDownload
 logger = logging.getLogger(__name__)
 
 
-class CLWebPoll(ModeleAvecMessageDAO):
+# Classe qui herite de ModeleAvecMessageDAO pour transmettre une transaction.
+class WebPageDownloadCommandLine(ModeleAvecMessageDAO):
 
     def __init__(self):
         super().__init__()
@@ -29,6 +29,11 @@ class CLWebPoll(ModeleAvecMessageDAO):
             help="Domaine de la transaction"
         )
 
+        self.parser.add_argument(
+            '--debug', action="store_true", required=False,
+            help="Active le debugging (logger)"
+        )
+
     def executer(self):
         downloader = WebPageDownload(self.configuration, self.message_dao)
         url = self.args.url
@@ -37,25 +42,6 @@ class CLWebPoll(ModeleAvecMessageDAO):
 
 
 # **** MAIN ****
-def main():
-    try:
-        demarreur.parse()
-
-        logging.basicConfig(level=logging.WARNING)
-#        logging.getLogger().setLevel(logging.DEBUG)
-        logging.getLogger(__name__).setLevel(logging.DEBUG)
-        logging.getLogger("mgdomaines").setLevel(logging.DEBUG)
-
-        demarreur.connecter()
-        demarreur.executer()
-    except Exception as e:
-        print("MAIN: Erreur fatale, voir log. Erreur %s" % str(e))
-        logger.exception("MAIN: Erreur")
-        demarreur.print_help()
-    finally:
-        demarreur.deconnecter()
-
-
 if __name__ == "__main__":
-    demarreur = CLWebPoll()
-    main()
+    web_downloader = WebPageDownloadCommandLine()
+    web_downloader.main()
