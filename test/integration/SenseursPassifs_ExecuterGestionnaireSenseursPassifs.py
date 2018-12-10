3e2922cb398f21df5ec54f5da9bc4cbeab7cc905
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-10 18:10:55 -0500

    Deplacement du code des domaines version consignation.

--- test/integration/SenseursPassifs_ExecuterGestionnaireSenseursPassifs.py
+++
@@ -1,8 +0,0 @@
-from mgdomaines.appareils.SenseursPassifs import GestionnaireSenseursPassifs
-
-gestionnaire = GestionnaireSenseursPassifs()
-
-gestionnaire.initialiser()
-gestionnaire.configurer()
-gestionnaire.traiter_backlog()
-gestionnaire.demarrer_traitement_messages_blocking(gestionnaire.get_nom_queue())
