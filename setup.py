7b61083bb744a58be2a6044dea21c04566c5f3c1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-10 12:22:43 -0500

    Update version 0.8.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MilleGrilles.domaines.python',
-    version='0.8.10',
+    version='0.8.11',
     packages=['mgdomaines',
               'mgdomaines.appareils',
               'mgdomaines.web',
